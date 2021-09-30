6a10972326f3211bfe3e2339fc437657903204df
Vladititov <capt.Hoffmann@gmail.com>
2021-09-30 12:19:36 +0300

    studied using services in middleware

---
+++ Task_07/Services/TimeService.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_07.Services
+{
+    public class TimeService
+    {
+        public TimeService() => 
+            Time = System.DateTime.Now.ToString("hh:mm:ss");
+        
+        public string Time { get; }
+
+    }
+}
---
+++ Task_07/TimerMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Task_07.Services;
+
+namespace Task_07
+{
+    public class TimerMiddleware
+    {
+        #region For AddTransient()
+        // private readonly RequestDelegate _next;
+        //
+        // private readonly TimeService _timeService;
+        //
+        // public TimerMiddleware(RequestDelegate next, TimeService timeService) =>
+        //     (_next, _timeService) = (next, timeService);
+        //
+        // public async Task InvokeAsync(HttpContext context)
+        // {
+        //     if (context.Request.Path.Value.ToLower().Equals("/time"))
+        //     {
+        //         context.Response.ContentType = "text/html; charset=utf-8";
+        //         await context.Response.WriteAsync($"Текущее время {_timeService?.Time}");
+        //     }
+        //     else
+        //     {
+        //         await _next.Invoke(context);
+        //     }
+        // } 
+        #endregion
+
+        private readonly RequestDelegate _next;
+
+        public TimerMiddleware(RequestDelegate next) => 
+            _next = next;
+
+        public async Task InvokeAsync(HttpContext context, TimeService timeService)
+        {
+            if (context.Request.Path.Value.ToLower().Equals("/time"))
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                await context.Response.WriteAsync($"Текущее время: {timeService?.Time}");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
