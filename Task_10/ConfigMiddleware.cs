ab51adc5844c820137e1357ef3966e5231dbd8bd
Vladititov <capt.Hoffmann@gmail.com>
2021-10-01 16:43:35 +0300

    studied how using Dependency Injection and file configuration providers

---
+++ Task_10/ConfigMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Task_10
+{
+    public class ConfigMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ConfigMiddleware(RequestDelegate next, IConfiguration config) =>
+            (_next, AppConfiguration) = (next, config);
+        
+        public IConfiguration AppConfiguration { get; set; }
+
+        public async Task InvokeAsync(HttpContext context) =>
+            await context.Response.WriteAsync($"name: {AppConfiguration["name"]} age: {AppConfiguration["age"]}");
+    }
+}
--- Task_10/Startup.cs
+++ Task_10/Startup.cs
@@ -13,38 +13,70 @@ namespace Task_10
 {
     public class Startup
     {
-        public Startup()
+        public Startup(IConfiguration config)
         {
+            #region Old builder
+            // var builder = new ConfigurationBuilder()
+            //     .AddJsonFile("conf.json")
+            //     .AddXmlFile("config.xml")
+            //     .AddIniFile("conf.ini");
+            #endregion
+            
             var builder = new ConfigurationBuilder()
-                .AddJsonFile("conf.json")
-                .AddXmlFile("config.xml")
-                .AddIniFile("conf.ini");
+                //.AddJsonFile("conf.json")
+                //AddEnvironmentVariables()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "name", "Tom" },
+                    { "age", "31"}
+                })
+                .AddConfiguration(config);
             
             AppConfiguration = builder.Build();
         }
 
         private IConfiguration AppConfiguration { get; set; }
 
-        public void ConfigureServices(IServiceCollection services) { }
-        
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
-            var colorJson = AppConfiguration["color-json"];
-            var textJson = AppConfiguration["text-json"];
-            var textSecond = AppConfiguration["namespace:class:method"];
-            
-            var colorXml = AppConfiguration["color-xml"];
-            var textXml = AppConfiguration["text-xml"];
+            services.AddTransient<IConfiguration>(provider => AppConfiguration);
+        }
+        
+        public void Configure(IApplicationBuilder app)
+        {
+            #region Old variables
 
-            var colorIni = AppConfiguration["color-ini"];
-            var textIni = AppConfiguration["text-ini"];
+            // var colorJson = AppConfiguration["color-json"];
+            // var textJson = AppConfiguration["text-json"];
+            // var textSecond = AppConfiguration["namespace:class:method"];
             
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync($"<p style='color:{colorJson}'>{textJson} - {textSecond}</p>");
-                await context.Response.WriteAsync($"<p style='color:{colorXml}'>{textXml}</p>");
-                await context.Response.WriteAsync($"<p style='color:{colorIni}'>{textIni}</p>");
-            });
+            // var colorXml = AppConfiguration["color-xml"];
+            // var textXml = AppConfiguration["text-xml"];
+
+            // var colorIni = AppConfiguration["color-ini"];
+            // var textIni = AppConfiguration["text-ini"];
+
+            #endregion
+
+            #region Old app.Run
+            // var color = AppConfiguration["color-json"];
+            // var text = AppConfiguration["name"];
+            //
+            // app.Run(async context =>
+            // {
+            //     #region Old WriteAsync
+            //     //await context.Response.WriteAsync($"<p style='color:{colorJson}'>{textJson} - {textSecond}</p>");
+            //     // await context.Response.WriteAsync($"<p style='color:{colorXml}'>{textXml}</p>");
+            //     // await context.Response.WriteAsync($"<p style='color:{colorIni}'>{textIni}</p>");
+            //     #endregion
+            //
+            //     await context.Response.WriteAsync($"<p style='color:{color};'>{text}</p>");
+            // });
+
+
+            #endregion
+
+            app.UseMiddleware<ConfigMiddleware>();
         }
     }
 }
