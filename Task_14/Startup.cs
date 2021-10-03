6158bad912381005b893eb7d904b6aaf0d0ba0c2
vladititov <capt.Hoffmann@gmail.com>
2021-10-03 19:51:20 +0300

    studied cookies in ASP.NET project

--- Task_14/Startup.cs
+++ Task_14/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,19 +17,51 @@ namespace Task_14
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Use(async (context, next) =>
-            {
-                context.Items["text"] = "Text from HttpContext.Items";
-                await next.Invoke();
-            });
+            #region For HttpContext.Items
+            // app.Use(async (context, next) =>
+            // {
+            //     context.Items["text"] = "Text from HttpContext.Items";
+            //     await next.Invoke();
+            // });
+            //
+            // app.Run(async context =>
+            // {
+            //     context.Response.ContentType = "text/html; charset=utf-8";
+            //     if (context.Items.ContainsKey("text"))
+            //         await context.Response.WriteAsync($"Текст: {context.Items["text"]}");
+            //     else
+            //         await context.Response.WriteAsync("Случайный текст!");
+            // });
+            #endregion
             
             app.Run(async context =>
             {
-                context.Response.ContentType = "text/html; charset=utf-8";
-                if (context.Items.ContainsKey("text"))
-                    await context.Response.WriteAsync($"Текст: {context.Items["text"]}");
+                string login;
+
+                if (context.Request.Cookies.TryGetValue("name", out login))
+                {
+                    await context.Response.WriteAsync($"Hello {login}");
+                }
                 else
-                    await context.Response.WriteAsync("Случайный текст!");
+                {
+                    context.Response.Cookies.Append("name", "Tom");
+                    await context.Response.WriteAsync($"Hello World!");
+                }
+
+                #region old code
+                // if (context.Request.Cookies.ContainsKey("name"))
+                // {
+                //     string name = context.Request.Cookies["name"];
+                //     await context.Response.WriteAsync($"Hello {name}");
+                // }
+                // else
+                // {
+                //     context.Response.Cookies.Append("name", "Tom");
+                //     await context.Response.WriteAsync("Hello World!");
+                // }
+
+                #endregion
+                
             });
         }
     }
