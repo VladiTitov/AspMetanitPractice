801ca1110082784c635d5bdce448a62ac591000c
Vladititov <capt.Hoffmann@gmail.com>
2021-10-04 15:07:15 +0300

    studied how filtering ILogger messages

---
+++ Task_16/Startup.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace Task_16
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddDebug();
+                builder.AddConsole();
+            
+                builder.AddFilter("System", LogLevel.Debug)
+                    .AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.Trace);
+            });
+            
+            ILogger logger = loggerFactory.CreateLogger<Startup>();
+
+
+            app.Run(async context =>
+            {
+                logger.LogInformation($"Requested Path: {context.Request.Path}");
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
