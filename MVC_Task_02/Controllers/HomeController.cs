2d04d201d94aa440f314a9e919d2c93cfa7a3d99
Vladititov <capt.Hoffmann@gmail.com>
2021-10-12 16:52:53 +0300

    Created first .NET ASP Core MVC project. Created empty .NET ASP Core project and added MVC services

---
+++ MVC_Task_02/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Task_02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
